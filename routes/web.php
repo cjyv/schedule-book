59f911b1d73a407bf3d33e63109b4000af05d630
cjy <cjyv1029@gmail.com>
2023-02-14 10:31:22 +0900

    home,login,regiser views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/login', function () {
+    return view('loginForm');
+});
+Route::get('/register', function () {
+    return view('register');
 });
