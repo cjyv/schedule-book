165dd80ed6ef89888394dad9f7e0b2034d1b4945
cjy <cjyv1029@gmail.com>
2023-02-27 17:51:25 +0900

    calendar design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,17 +16,145 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 <!--カレンダー -->
-<script src='https://cdn.jsdelivr.net/npm/fullcalendar/index.global.min.js'></script>
+<link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/main.min.css' rel='stylesheet' />
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/main.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/locales-all.min.js'></script>	
+<script class="cssdesk" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.0/moment.min.js" type="text/javascript"></script>
+
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
-        const calendarEl = document.getElementById('calendar')
-        const calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        })
-        calendar.render()
-      })
+		var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+			
+			initialView: 'dayGridMonth',
+			selectable: true,// 달력 일자 드래그 설정가능
+			navLinks: true, // 날짜를 선택하면 Day 캘린더나 Week 캘린더로 링크
+			editable: true, // 수정 가능
+			nowIndicator: true, // 현재 시간 마크
+			dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
+			locale: 'ja', // 한국어 설정
+			eventAdd: function(obj) { // 이벤트가 추가되면 발생하는 이벤트
+				console.log('add');
+				
+			},
+			eventChange: function(obj) { // 이벤트가 수정되면 발생하는 이벤트
+				
+				// GMT 시간은 9시간 빨라서 9시간 빼줘야됨
+				var start = obj.event._instance.range.start;
+				if(start.getHours() == 9) {
+					start = moment(start).format('YYYY-MM-DD') + " 00:00";
+				}
+				else {
+					start = start.setHours(start.getHours() - 9);
+					start = moment(start).format('YYYY-MM-DD hh:mm');
+				}
+				
+				
+				var end = obj.event._instance.range.end;
+				if(end.getHours() == 9) {
+					end = moment(end).format('YYYY-MM-DD') + " 00:00";
+				}
+				else {
+					end = end.setHours(end.getHours() - 9);
+					end = moment(end).format('YYYY-MM-DD hh:mm');
+				}
+				
+				
+				$.ajax({
+						  url: "/~team2/admin/ajax_calendar_edit",
+						  type: "POST",
+						  data : {
+								title : obj.event._def.title,
+								start: start,
+								end: end
+						  },
+						  traditional: true,
+						  async: false, //동기
+						  success : function(data){
+							  
+						  },
+						  error : function(request,status,error){
+							alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+							console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
+						  }
+					});
+				
+			},
+			eventRemove: function(obj){ // 이벤트가 삭제되면 발생하는 이벤트
+				console.log('remove');
+				
+			},
+			select: function(arg) { // 캘린더에서 드래그로 이벤트 생성
+				
+				var title = prompt('日程入力:');
+				if (title) {
+					$.ajax({
+						  url: "/~team2/admin/ajax_calendar_add",
+						  type: "POST",
+						  data : {
+								title: title,
+								start: arg.startStr,
+								end: arg.endStr,
+								allDay: arg.allDay
+						  },
+						  traditional: true,
+						  async: false, //동기
+						  success : function(data){
+							  
+						  },
+						  error : function(request,status,error){
+							alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+							console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
+						  }
+					});
 
+					calendar.addEvent({
+						title: title,
+						start: arg.startStr,
+						end: arg.endStr,
+						allDay: arg.allDay
+					})
+				} 
+				
+				calendar.unselect()
+			},
+			droppable: true,
+			eventClick: function(arg) { 
+				// 있는 일정 클릭시, 
+				console.log(arg);
+				if (confirm('日程を削除しますか?')) 
+				{ 
+					$.ajax({
+						  url: "/~team2/admin/ajax_calendar_delete",
+						  type: "POST",
+						  data : {
+								title : arg.event._def.title
+						  },
+						  traditional: true,
+						  async: false, //동기
+						  success : function(data){
+							 
+						  },
+						  error : function(request,status,error){
+							alert("code = "+ request.status + " message = " + request.responseText + " error = " + error); // 실패 시 처리
+							console.log("code = "+ request.status + " message = " + request.responseText + " error = " + error);
+						  }
+					});
+					arg.event.remove();
+				} 
+			},
+			eventBorderColor : '#82d1ff', // 이벤트 테두리색
+			eventBackgroundColor : '#82d1ff' , // 이벤트 배경색
+			headerToolbar: { // 툴바
+				left: 'prev,next today',
+				center: 'title',
+				right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+			}
+		
+		});
+        calendar.render();
+	});
     </script>
 </head>
 <body>
